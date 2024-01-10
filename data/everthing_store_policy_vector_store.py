ea49f72d157492b4bb8b3da12b62c6fc09e91659
Anas Khan <anas985822@gmail.com>
2024-01-10 23:49:21 +0500

    faiss policy doc

--- data/everthing_store_policy_vector_store.py
+++ data/everthing_store_policy_vector_store.py
@@ -1,49 +1,15 @@
-import openai
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings.openai import OpenAIEmbeddings
 
-from llama_index import ServiceContext, LLMPredictor, OpenAIEmbedding, PromptHelper
-from llama_index.llms import OpenAI
-from llama_index.text_splitter import TokenTextSplitter
-from llama_index.node_parser import SimpleNodeParser
-from llama_index.readers import PDFReader
-import tiktoken
-from llama_index.indices.vector_store.base import VectorStoreIndex
+loader = PyPDFLoader("/home/khudi/Desktop/EcommerceAgentProject/EverthingStore-policy.pdf")
+pages = loader.load_and_split()
 
-llm = OpenAI(model='gpt-3.5-turbo', temperature=0, max_tokens=256)
 
-embed_model = OpenAIEmbedding()
-text_splitter = TokenTextSplitter(
-  separator=" ",
-  chunk_size=1024,
-  chunk_overlap=20,
-  backup_separators=["\n"],
-  tokenizer=tiktoken.encoding_for_model("gpt-3.5-turbo").encode
-)
 
-node_parser = SimpleNodeParser.from_defaults(
-  text_splitter=text_splitter
-)
-prompt_helper = PromptHelper(
-  context_window=4096, 
-  num_output=256, 
-  chunk_overlap_ratio=0.1, 
-  chunk_size_limit=None
-)
-
-service_context = ServiceContext.from_defaults(
-  llm=llm,
-  embed_model=embed_model,
-  node_parser=node_parser,
-  prompt_helper=prompt_helper
-)
-
-documents = PDFReader(file='/home/khudi/Desktop/EcommerceAgentProject/EverthingStore-policy.pdf').load_data()
-
-index = VectorStoreIndex.from_documents(
-    documents, 
-    service_context = service_context
-    )
-index.storage_context.persist('./policy')
-
-query_engine = index.as_query_engine(service_context=service_context)
-response = query_engine.query("What is HNSW?")
-print(response)
+faiss_index = FAISS.from_documents(pages, OpenAIEmbeddings())
+faiss_index.save_local('./faiss_index')
+docs = faiss_index.similarity_search("what is the reutrn policy?", k=2)
+print(docs)
+# for doc in docs:
+#     print(str(doc.metadata["page"]) + ":", doc.page_content[:300])
